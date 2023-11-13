dd86f64527fc5fde2703a58a66ef6c0d6ecf4b83
Nelson Rosales <nelson.rosales83@gmail.com>
2023-11-13 10:13:18 +0100

    feat: women and men page

--- src/products/pages/MensPage.tsx
+++ src/products/pages/MensPage.tsx
@@ -1,12 +1,19 @@
-import { ProductList } from ".."
+import { ProductList, useProducts } from ".."
 
 export const MensPage = () => {
+  const { isLoading, products } = useProducts({
+    filterKey: "men's clothing",
+  })
+
   return (
     <div className="flex-col">
       <h1 className="text-2xl font-bold">Productos para hombres</h1>
 
-      <ProductList />
+      <p className="text-gray-500 mb-5">
+        {isLoading ? "Cargando..." : `Hay ${products.length} productos`}
+      </p>
 
+      <ProductList products={products} />
     </div>
   )
 }
--- src/products/services/actions.ts
+++ src/products/services/actions.ts
@@ -5,7 +5,19 @@ interface GetProductsOptions {
   filterKey?: string
 }
 
+const sleep = (seconds: number = 2): Promise<boolean> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true)
+    }, seconds * 1000)
+  })
+}
+
 export const getProducts = async ({ filterKey }: GetProductsOptions) => {
-  const { data } = await productsApi.get<Product[]>(`/products`)
+  await sleep(2)
+
+  const filterUrl = filterKey ? `?category=${filterKey}` : ""
+
+  const { data } = await productsApi.get<Product[]>(`/products${filterUrl}`)
   return data
 }
